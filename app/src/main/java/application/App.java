4fca2f58f36ccafe08856d532b7651203cab80a0
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2024-03-08 01:36:11 +0000

    Aula 07/03/2024

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -1,7 +1,16 @@
 package application;
 
+import application.model.Tarefa;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!!!");
+        Tarefa t = new Tarefa();
+        t.setDescricao("Teste de Tarefa");
+        t.setConcluido(false);
+
+        System.out.println(t.getDescricao());
+
+        Tarefa t2 = new Tarefa("Segunda Tarefa");
+        System.out.println(t2.getDescricao());
     }
 }
---
+++ app/src/main/java/application/model/Tarefa.java
@@ -0,0 +1,27 @@
+package application.model;
+
+public class Tarefa extends AbstractAtividade {
+    private boolean concluido;
+
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+    public boolean getConcluido() {
+        return this.concluido;
+    }
+
+    public Tarefa(String descricao, boolean concluido) {
+        this.setDescricao(descricao);
+        this.setConcluido(concluido);
+    }
+    public Tarefa() {}
+    public Tarefa(String descricao) {
+        this.setDescricao(descricao);
+        this.setConcluido(false);
+    }
+
+    @Override
+    public String getDetalhes() {
+        return "[T] " + this.getDescricao();
+    }
+}
